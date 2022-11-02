86280a5bec6e6e76f39580c831432d4af22475df
Zeosleus <zeosleus@protonmail.com>
2022-11-02 23:09:31 +0200

    More work on write_density_mpi()

--- Ex1 - MPI and PDEs/hpc_ex01_code/mpiio/diffusion2d_mpi_square_nb_io.c
+++ Ex1 - MPI and PDEs/hpc_ex01_code/mpiio/diffusion2d_mpi_square_nb_io.c
@@ -326,11 +326,11 @@ void write_diagnostics(Diffusion2D *D2D, const char *filename)
 
 void write_density_mpi(Diffusion2D *D2D, char *filename)
 {
-        int ntot_ = D2D->Ntot_;
+        int local_N_ = D2D->local_N_;
         int rank_ = D2D->rank_;
         double* rho_ = D2D->rho_;
 
-        // Open the file
+        // Open the file (collective call)
         MPI_File f;
         MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f);
         MPI_File_set_size(f, 0);
@@ -338,11 +338,13 @@ void write_density_mpi(Diffusion2D *D2D, char *filename)
         // Calculate the offset for each rank
         MPI_Offset base;
         MPI_File_get_position(f, &base);
-        base = base + rank_ * ntot_;
+
+        int data_len = (local_N_ * local_N_) * sizeof(double);
+        MPI_Offset offset = rank_ * data_len;
 
         // Write the data
         MPI_Status status;
-        MPI_File_write_at_all(f, base, rho_, ntot_, MPI_DOUBLE, &status);
+        MPI_File_write_at_all(f, base + offset, rho_, local_N_ * local_N_, MPI_DOUBLE, &status); // blocking collective call
 
         // Close the file
         MPI_File_close(&f);
