b9ea57461933bb4af3a847ad8e3d16e07768dba1
VasMil <v_milionis@yahoo.com>
2023-01-30 01:07:57 +0200

    Loads training data in blocks and for each block updates all query points' nearest neighbors, so we make better use of L1d cache

---
+++ Project - kNN/blocking/func.h
@@ -0,0 +1,330 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define NNBS 32
+
+// struct that will preserve the k nearest neighbors for each query.
+// Will be used in order to load training data in a blocking fashion,
+// iterating over all query points. Thus for each query point we will
+// need to preserve the k nearest neighbors that have been found so far
+// in the preceding blocks.
+typedef struct query_s
+{
+	double *x; // Query's coordinates + surrogate
+	int nn_idx[NNBS]; // The index (< TRAINELEMS) of the k nearest neighbors
+	double nn_d[NNBS]; // The distance between the query point and each one of the k nearest neighbors
+} query_t;
+
+/* I/O routines */
+void store_binary_data(char *filename, double *data, int n)
+{
+	FILE *fp;
+	fp = fopen(filename, "wb");
+	if (fp == NULL)
+	{
+		printf("fopen(%s, \"wb\") FAILED!\n", filename);
+		exit(1);
+	}
+	size_t nelems = fwrite(data, sizeof(double), n, fp);
+	assert(nelems == n); // check that all elements were actually written
+	fclose(fp);
+}
+
+void load_binary_data(const char *filename, double *data, query_t *query_status, const int n)
+{
+	FILE *fp;
+	fp = fopen(filename, "rb");
+	if (fp == NULL)
+	{
+		printf("fopen(%s, \"wb\") FAILED!\n", filename);
+		exit(1);
+	}
+	size_t nelems = fread(data, sizeof(double), n, fp);
+	assert(nelems == n); // check that all elements were actually read
+	fclose(fp);
+
+	// If queries are loaded, initialize the query_status structs
+	if (query_status != NULL)
+	{
+		for (int i = 0; i < QUERYELEMS; i++)
+		for (int j = 0; j < NNBS; j++)
+		{
+			query_status[i].x = &data[i*(PROBDIM+1)];
+			query_status[i].nn_idx[j] = -1;
+			query_status[i].nn_d[j] = 1e99 - j;
+		}
+	}
+}
+
+double read_nextnum(FILE *fp)
+{
+	double val;
+
+	int c = fscanf(fp, "%lf", &val);
+	if (c <= 0) {
+		fprintf(stderr, "fscanf returned %d\n", c);
+		exit(1);
+	}
+	return val;
+}
+
+/* Timer */
+double gettime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, 0);
+	return (double) (tv.tv_sec+tv.tv_usec/1000000.0);
+}
+
+/* Function to approximate */
+double fitfun(double *x, int n)
+{
+	double f = 0.0;
+	int i;
+
+#if 1
+	for(i=0; i<n; i++)	/* circle */
+		f += x[i]*x[i];
+#endif
+#if 0
+	for(i=0; i<n-1; i++) {	/*  himmelblau */
+		f = f + pow((x[i]*x[i]+x[i+1]-11.0),2) + pow((x[i]+x[i+1]*x[i+1]-7.0),2);
+	}
+#endif
+#if 0
+	for (i=0; i<n-1; i++)   /* rosenbrock */
+		f = f + 100.0*pow((x[i+1]-x[i]*x[i]),2) + pow((x[i]-1.0),2);
+#endif
+#if 0
+	for (i=0; i<n; i++)     /* rastrigin */
+		f = f + pow(x[i],2) + 10.0 - 10.0*cos(2*M_PI*x[i]);
+#endif
+
+	return f;
+}
+
+
+/* random number generator  */
+#define SEED_RAND()     srand48(1)
+#define URAND()         drand48()
+
+#ifndef LB
+#define LB -1.0
+#endif
+#ifndef UB
+#define UB 1.0
+#endif
+
+double get_rand(int k)
+{
+	return (UB-LB)*URAND()+LB;
+}
+
+
+/* utils */
+double compute_min(double *v, int n)
+{
+	int i;
+	double vmin = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] < vmin) vmin = v[i];
+
+	return vmin;
+}
+
+double compute_max(double *v, int n)
+{
+	int i;
+	double vmax = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] > vmax) vmax = v[i];
+
+	return vmax;
+}
+
+double compute_sum(double *v, int n)
+{
+	int i;
+	double s = 0;
+	for (i = 0; i < n; i++) s += v[i];
+
+	return s;
+}
+
+double compute_sum_pow(double *v, int n, int p)
+{
+	int i;
+	double s = 0;
+	for (i = 0; i < n; i++) s += pow(v[i], p);
+
+	return s;
+}
+
+double compute_mean(double *v, int n)
+{
+	int i;
+	double s = 0;
+	for (i = 0; i < n; i++) s += v[i];
+
+	return s/n;
+}
+
+double compute_std(double *v, int n, double mean)
+{
+	int i;
+	double s = 0;
+	for (i = 0; i < n; i++) s += pow(v[i]-mean,2);
+
+	return sqrt(s/(n-1));
+}
+
+double compute_var(double *v, int n, double mean)
+{
+	int i;
+	double s = 0;
+	for (i = 0; i < n; i++) s += pow(v[i]-mean,2);
+
+	return s/n;
+}
+
+double compute_dist(double *v, double *w, int n)
+{
+	int i;
+	double s = 0.0;
+	for (i = 0; i < n; i++) {
+		s += pow(v[i]-w[i],2);
+	}
+
+	return sqrt(s);
+}
+
+double compute_max_pos(double *v, int n, int *pos)
+{
+	int i, p = 0;
+	double vmax = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] > vmax) {
+			vmax = v[i];
+			p = i;
+		}
+
+	*pos = p;
+	return vmax;
+}
+
+double compute_min_pos(double *v, int n, int *pos)
+{
+	int i, p = 0;
+	double vmin = v[0];
+	for (i = 1; i < n; i++)
+		if (v[i] < vmin) {
+			vmin = v[i];
+			p = i;
+		}
+
+	*pos = p;
+	return vmin;
+}
+
+double compute_root(double dist, int norm)
+{
+	if (dist == 0) return 0;
+
+	switch (norm) {
+	case 2:
+		return sqrt(dist);
+	case 1:
+	case 0:
+		return dist;
+	default:
+		return pow(dist, 1 / (double) norm);
+	}
+}
+
+double compute_distance(double *pat1, double *pat2, int lpat, int norm)
+{
+	register int i;
+	double dist = 0.0;
+
+	for (i = 0; i < lpat; i++) {
+		double diff = 0.0;
+
+		diff = pat1[i] - pat2[i];
+
+		switch (norm) {
+		double   adiff;
+
+		case 2:
+			dist += diff * diff;
+			break;
+		case 1:
+			dist += fabs(diff);
+			break;
+		case 0:
+			if ((adiff = fabs(diff)) > dist)
+			dist = adiff;
+			break;
+		default:
+			dist += pow(fabs(diff), (double) norm);
+			break;
+		}
+	}
+
+	return dist;	// compute_root(dist);
+}
+
+// npat -> TRAINELEMS
+// lpat -> PROBDIM
+void compute_knn_brute_force(double **xdata, query_t *q, int dim, int k, int train_data_offset, int num_train_data)
+{
+	int i, max_i;
+	double max_d, new_d;
+
+	max_d = compute_max_pos(q->nn_d, k, &max_i);
+	for (i = train_data_offset; i < train_data_offset + num_train_data; i++)
+	{
+		new_d = compute_dist(q->x, xdata[i], dim); // euclidean
+		if (new_d < max_d) // add point to the list of knns, replace element max_i
+		{	
+			q->nn_idx[max_i] = i;
+			q->nn_d[max_i] = new_d;
+		}
+		max_d = compute_max_pos(q->nn_d, k, &max_i);
+	}
+
+/* 	// sort the knn list using bubble sort
+	int temp_idx, j;
+	double temp_d;
+	for (i = (k - 1); i > 0; i--)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			if (q->nn_d[j-1] > q->nn_d[j])
+			{
+				temp_d = q->nn_d[j-1];
+				q->nn_d[j-1] = q->nn_d[j];
+				q->nn_d[j] = temp_d;
+				
+				temp_idx = q->nn_idx[j-1];
+				q->nn_idx[j-1] = q->nn_idx[j];
+				q->nn_idx[j] = temp_idx;
+			}
+		}
+	} */
+}
+
+
+/* compute an approximation based on the values of the neighbors */
+double predict_value(int dim, int knn, double *xdata, double *ydata)
+{
+	int i;
+	double sum_v = 0.0;
+	// plain mean (other possible options: inverse distance weight, closest value inheritance)
+	for (i = 0; i < knn; i++)
+		sum_v += ydata[i];
+
+	return sum_v / knn;
+}
---
+++ Project - kNN/blocking/myknn.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include "func.h"
+
+#ifndef PROBDIM
+#define PROBDIM 2
+#endif
+
+#define DEBUG 1
+
+// My L1d = 192 KiB, and I want cache the maximum amount of training points
+// each of size PROBDIM * sizeof(double) = 16 * 8 = 128 bytes.
+// Thus I may preserve in L1d cache 192,000 / 128 = 1,500 points simultaneously.
+// I also need to be able to store in cache the query point aswell (!!)
+// and have a block size that will evenly devide TRAINELEMS.
+// The easy solution is to get the max power of 2 that is less that 1,500,
+// since TRAINELEMS is also a power of 2.
+#define TRAIN_BLOCK_SIZE 1024
+
+static double **xdata;
+static double ydata[TRAINELEMS];
+
+// #define MAX_NNB	256
+
+double find_knn_value(query_t *q, int knn)
+{
+	double xd[knn * PROBDIM];     // the knn neighboring points/vectors of size PROBDIM
+	double fd[knn];	      	      // function values for the knn neighbors
+
+	for (int i = 0; i < knn; i++)
+		fd[i] = ydata[q->nn_idx[i]];
+
+	for (int i = 0; i < knn; i++) 
+		for (int j = 0; j < PROBDIM; j++)
+			xd[i*PROBDIM + j] = xdata[q->nn_idx[i]][j];
+
+	return predict_value(PROBDIM, knn, xd, fd);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("usage: %s <trainfile> <queryfile>\n", argv[0]);
+		exit(1);
+	}
+
+	char *trainfile = argv[1];
+	char *queryfile = argv[2];
+	double *mem = (double *)malloc(TRAINELEMS * (PROBDIM + 1) * sizeof(double));
+	double *query_mem = (double *)malloc(QUERYELEMS * (PROBDIM + 1) * sizeof(double));
+	query_t *queries = (query_t *)malloc(QUERYELEMS * sizeof(query_t));
+	
+	xdata = (double **)malloc(TRAINELEMS * sizeof(double *));
+	
+	load_binary_data(trainfile, mem, NULL, TRAINELEMS*(PROBDIM+1));
+
+	for (int i = 0; i < TRAINELEMS; i++)
+		xdata[i] = mem + i * (PROBDIM + 1); //&mem[i*PROBDIM];
+
+	for (int i = 0; i < TRAINELEMS; i++)
+	{
+#if defined(SURROGATES)
+		ydata[i] = mem[i * (PROBDIM + 1) + PROBDIM];
+#else
+		ydata[i] = 0;
+#endif
+	}
+
+	load_binary_data(queryfile, query_mem, queries, QUERYELEMS * (PROBDIM + 1));
+
+#if DEBUG
+	FILE *fpout = fopen("output.knn.txt","w");
+#endif
+	double *y = malloc(QUERYELEMS * sizeof(double));
+
+	double t0, t1;
+	double sse = 0.0;
+	double err, err_sum = 0.0;
+	
+	assert(TRAINELEMS % TRAIN_BLOCK_SIZE == 0);
+
+	t0 = gettime();
+	for (int train_offset = 0; train_offset < TRAINELEMS; train_offset += TRAIN_BLOCK_SIZE)
+	{
+		for (int i = 0; i < QUERYELEMS; i++)
+		{	/* requests */
+#if defined(SURROGATES)
+			y[i] = query_mem[i * (PROBDIM + 1) + PROBDIM];
+#else
+			y[i] = 0.0;
+#endif
+			compute_knn_brute_force(xdata, &(queries[i]), PROBDIM, NNBS, train_offset, TRAIN_BLOCK_SIZE);
+		}
+	}
+	t1 = gettime();
+
+	for (int i = 0; i < QUERYELEMS; i++)
+	{
+		double yp = find_knn_value(&(queries[i]), NNBS);
+		sse += (y[i] - yp) * (y[i] - yp);
+		err = 100.0 * fabs((yp - y[i]) / y[i]);
+
+#if DEBUG
+		fprintf(fpout,"%.5f %.5f %.2f\n", y[i], yp, err);
+#endif
+		err_sum += err;
+	}
+
+	double mse = sse / QUERYELEMS;
+	double ymean = compute_mean(y, QUERYELEMS);
+	double var = compute_var(y, QUERYELEMS, ymean);
+	double r2 = 1 - (mse / var);
+
+	printf("Results for %d query points\n", QUERYELEMS);
+	printf("APE = %.2f %%\n", err_sum / QUERYELEMS);
+	printf("MSE = %.6f\n", mse);
+	printf("R2 = 1 - (MSE/Var) = %.6lf\n", r2);
+
+	printf("Total time = %lf ms\n", t1 - t0);
+	printf("Average time/query = %lf ms\n", (t1 - t0) / QUERYELEMS);
+
+	free(mem);
+	free(xdata);
+	free(query_mem);
+	free(queries);
+	free(y);
+
+	return 0;
+}
